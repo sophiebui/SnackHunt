592216cd8fc939caf27a1dace25015386ad3a57b
Sophia <sophiatbui@gmail.com>
2022-01-06 12:05:58 -0700

    api router tested & completed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken())
-    res.send('Hello World!')
-})
+	res.cookie('XSRF-TOKEN', req.csrfToken());
+	res.send('Hello World!');
+});
 
 module.exports = router;
-
-
