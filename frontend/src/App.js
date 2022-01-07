fef1ccc2a92e6d2c79add92304fd6827ac4a1cdd
Sophia <sophiatbui@gmail.com>
2022-01-07 16:02:41 -0700

    phase 1 done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
-import LoginFormPage from './components/LoginFormPage';
-import { Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
 
 function App() {
-	return (
-		<div>
-			<Switch>
-				<Route exact path="/">
-					<h1>Hello from App</h1>
-				</Route>
-				<Route path="/login">
-					<LoginFormPage />
-				</Route>
-			</Switch>
-		</div>
-	);
-}
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
 
+  return isLoaded && (
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
+  );
+}
 export default App;
