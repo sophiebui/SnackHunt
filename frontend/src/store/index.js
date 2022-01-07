9185b271d0a00688ee5a6fd145092a2fd7ebc253
Sophia <sophiatbui@gmail.com>
2022-01-06 20:21:11 -0700

    restore xsrf token cookie

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { restoreCSRF, csrfFetch } from './csrf';
+
 
 const rootReducer = combineReducers({
   // add reducer functions here
@@ -12,12 +14,21 @@ if (process.env.NODE_ENV === "production") {
 } else {
   const logger = require("redux-logger").default;
   const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 export default configureStore;
