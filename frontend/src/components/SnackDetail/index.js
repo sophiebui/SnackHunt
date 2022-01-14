405b78fa3d05ce9479c2e0906386d39496e2ea27
Sophia <sophiatbui@gmail.com>
2022-01-13 23:43:39 -0700

    added links back to my snacks and styling

--- frontend/src/components/SnackDetail/index.js
+++ frontend/src/components/SnackDetail/index.js
@@ -1,11 +1,10 @@
 import { useSelector } from 'react-redux';
-
-import { useParams } from 'react-router-dom';
-
+import {Link, useParams } from 'react-router-dom';
 import './SnackDetail.css';
 
 const SnackDetail = () => {
 	const sessionSnacks = useSelector((state) => state.snacks);
+	const sessionUser = useSelector((state) => state.session.user);
 	const snackArr = Object.values(sessionSnacks.entries);
 	const { snackId } = useParams();
 
@@ -25,7 +24,15 @@ const SnackDetail = () => {
 					</div>
 					<div className='snack-detail-text-div'>
 						<h2 className='snack-detail-h2'>{snack.title}</h2>
-						<p className='snack-detail-p'>{snack.description}</p>
+						<p className='snack-detail-p-desc'>{snack.description}</p>
+							{+snack.ownerId === +sessionUser.id ?
+							<>
+							<p className='your-snack-p'>Recognize this snack? It's one of yours! View all of your munchies
+							<Link to={`/${snack.ownerId}`} className='link-to-my-snacks'> here</Link>.
+							</p>
+							</>
+							: null}
+							<Link to='/' className='home-link'>Back to Home</Link>
 					</div>
 				</li>
 			))}
